608921d8c1f00983c8b9e875bb434105ef727702
TheGreatPintoJ <jukujunk@gmail.com>
2025-02-22 18:53:21 -0500

    Added 'connectWifi' function

---
+++ DIYWatch/wifi.cpp
@@ -0,0 +1,18 @@
+void connectWifi(String ssid, String password){
+  Serial.println();
+  Serial.println("******************************************************");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
---
+++ DIYWatch/wifi.h
@@ -0,0 +1,6 @@
+#ifndef WIFI_H
+#define WIFI_H
+
+void connectWifi(String ssid, String password);
+
+#endif
