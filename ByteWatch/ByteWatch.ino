b6ba173bdb6c138fec141baf1146c7849177b734
TheGreatPintoJ <jukujunk@gmail.com>
2025-02-25 13:16:04 -0500

    Added tasks

--- ByteWatch/ByteWatch.ino
+++ ByteWatch/ByteWatch.ino
@@ -1,15 +1,16 @@
+#include <Arduino.h>
+
 #include "pins.h"
 #include "wifi_functs.h"
 #include "config.h"
 #include "miscutils.h"
-#include "display_setup.h"
+//#include "display_setup.h"
+#include "updatevalueutils.h"
 
-void setup() {
-  // put your setup code here, to run once:
-  
+void setup(){
+  Serial.begin(9600);
+  xTaskCreatePinnedToCore(updateBatteryValue, "Update Battery Value", 2048, NULL, 1, &updateBatteryTask, 0);
+  xTaskCreatePinnedToCore(updateWifiValue, "Update Wifi Signal", 2048, NULL, 1, &updateWifiTask, 1);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  
-}
+void loop(){}
