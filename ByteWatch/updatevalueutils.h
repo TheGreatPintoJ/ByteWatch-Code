62a7503c33c56e6db2b2a7728b3772901b5e3efd
TheGreatPintoJ <jukujunk@gmail.com>
2025-02-25 20:38:59 -0500

    Removed task variables

--- ByteWatch/updatevalueutils.h
+++ ByteWatch/updatevalueutils.h
@@ -1,8 +1,6 @@
 #ifndef UPDATEVALUEUTILS_H
 #define UPDATEVALUEUTILS_H
 
-TaskHandle_t updateBatteryTask, updateWifiTask;
-
 void updateBatteryValue(void *parameter);
 void updateWifiValue(void *parameter);
 
