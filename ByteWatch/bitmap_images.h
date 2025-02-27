10bd51bcba6503ec8807f62320bc20f94d579897
TheGreatPintoJ <jukujunk@gmail.com>
2025-02-27 15:25:51 -0500

    Removed PROGMEM tags that were causing errors

--- ByteWatch/bitmap_images.h
+++ ByteWatch/bitmap_images.h
@@ -1,41 +1,41 @@
 #ifndef BITMAP_IMAGES_H
 #define BITMAP_IMAGES_H
 
-extern const unsigned char PROGMEM image_bluetooth_off_bits[];
-extern const unsigned char PROGMEM image_bluetooth_on_bits[];
-extern const unsigned char PROGMEM image_bluetooth_connected_bits[];
+extern const unsigned char image_bluetooth_off_bits[];
+extern const unsigned char image_bluetooth_on_bits[];
+extern const unsigned char image_bluetooth_connected_bits[];
 
-extern const unsigned char PROGMEM image_wifi_off_bits[];
-extern const unsigned char PROGMEM image_wifi_nosig_bits[];
-extern const unsigned char PROGMEM image_wifi_1_bars_bits[];
-extern const unsigned char PROGMEM image_wifi_2_bars_bits[];
-extern const unsigned char PROGMEM image_wifi_3_bars_bits[];
-extern const unsigned char PROGMEM image_wifi_4_bars_bits[];
+extern const unsigned char image_wifi_off_bits[];
+extern const unsigned char image_wifi_nosig_bits[];
+extern const unsigned char image_wifi_1_bars_bits[];
+extern const unsigned char image_wifi_2_bars_bits[];
+extern const unsigned char image_wifi_3_bars_bits[];
+extern const unsigned char image_wifi_4_bars_bits[];
 
-extern const unsigned char PROGMEM image_volume_no_sound_bits[];
-extern const unsigned char PROGMEM image_volume_muted_bits[];
-extern const unsigned char PROGMEM image_volume_low_bits[];
-extern const unsigned char PROGMEM image_volume_normal_bits[];
-extern const unsigned char PROGMEM image_volume_loud_bits[];
+extern const unsigned char image_volume_no_sound_bits[];
+extern const unsigned char image_volume_muted_bits[];
+extern const unsigned char image_volume_low_bits[];
+extern const unsigned char image_volume_normal_bits[];
+extern const unsigned char image_volume_loud_bits[];
 
-extern const unsigned char PROGMEM image_battery_charging_bits[];
-extern const unsigned char PROGMEM image_battery_100_bits[];
-extern const unsigned char PROGMEM image_battery_80_bits[];
-extern const unsigned char PROGMEM image_battery_70_bits[];
-extern const unsigned char PROGMEM image_battery_60_bits[];
-extern const unsigned char PROGMEM image_battery_50_bits[];
-extern const unsigned char PROGMEM image_battery_40_bits[];
-extern const unsigned char PROGMEM image_battery_30_bits[];
-extern const unsigned char PROGMEM image_battery_20_bits[];
-extern const unsigned char PROGMEM image_battery_10_bits[];
-extern const unsigned char PROGMEM image_battery_0_bits[];
+extern const unsigned char image_battery_charging_bits[];
+extern const unsigned char image_battery_100_bits[];
+extern const unsigned char image_battery_80_bits[];
+extern const unsigned char image_battery_70_bits[];
+extern const unsigned char image_battery_60_bits[];
+extern const unsigned char image_battery_50_bits[];
+extern const unsigned char image_battery_40_bits[];
+extern const unsigned char image_battery_30_bits[];
+extern const unsigned char image_battery_20_bits[];
+extern const unsigned char image_battery_10_bits[];
+extern const unsigned char image_battery_0_bits[];
 
 
-extern const unsigned char PROGMEM image_message_mail_bits[];
-extern const unsigned char PROGMEM image_notification_bits[];
+extern const unsigned char image_message_mail_bits[];
+extern const unsigned char image_notification_bits[];
 
-extern const unsigned char PROGMEM image_menu_options_bits[];
+extern const unsigned char image_menu_options_bits[];
 
-extern const unsigned char PROGMEM image_phone_call_incoming_bits[];
+extern const unsigned char image_phone_call_incoming_bits[];
 
 #endif
